d12c6b4af23f032e7faec0bc2025d23c5fb9efa9
Sriram <sriramnvilvanathan@gmail.com>
2024-07-01 15:19:22 +0530

    added classes for ball and player, created basic game manager

--- 3/main.cpp
+++ 3/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
+
 using namespace std;
 
 enum eDirection
@@ -87,30 +91,96 @@ public:
     }
 };
 
+class cPaddle
+{
+private:
+    int x, y;
+    int originalX, originalY;
+
+public:
+    cPaddle()
+    {
+        x = y = 0;
+    }
+    cPaddle(int posX, int posY) : cPaddle()
+    {
+        originalX = posX;
+        originalY = posY;
+        x = posX;
+        y = posY;
+    }
+    inline void Reset()
+    {
+        x = originalX;
+        y = originalY;
+    }
+    inline int getX() { return x; }
+    inline int getY() { return y; }
+    inline void moveUp() { y--; }
+    inline void moveDown() { y++; }
+
+    friend ostream &operator<<(ostream &o, cPaddle c)
+    {
+        o << "PADDLE [" << c.x << "," << c.y << "]" << endl;
+        return o;
+    }
+};
+
+class cGameManager
+{
+private:
+    int width, height;
+    int score1, score2;
+    char up1, down1, up2, down2;
+    bool quit;
+    cBall *ball;
+    cPaddle *p1;
+    cPaddle *p2;
+
+public:
+    cGameManager(int w, int h)
+    {
+        srand(time(NULL));
+        quit = false;
+        up1 = 'w';
+        down1 = 's';
+        up2 = 'i';
+        down2 = 'k';
+        score1 = score2 = 0;
+        width = w;
+        height = h;
+        ball = new cBall(w / 2, h / 2);
+        p1 = new cPaddle(1, h / 2 - 3);
+        p2 = new cPaddle(w - 2, h / 2 - 3);
+    }
+
+    ~cGameManager()
+    {
+        delete ball, p1, p2;
+    }
+
+    void scoreUp(cPaddle *player)
+    {
+        if (player == p1)
+            score1++;
+        else if (player == p2)
+            score2++;
+
+        ball->Reset();
+        p1->Reset();
+        p2->Reset();
+    }
+    void Draw()
+    {
+        system("cls");
+    }
+
+    void Input() {}
+    void Logic() {}
+};
+
 int main()
 {
-    cBall c(0, 0);
-    cout << c << endl;
-    c.randomDirection();
-    cout << c << endl;
-    c.Move();
-    cout << c << endl;
-    c.randomDirection();
-    cout << c << endl;
-    c.Move();
-    cout << c << endl;
-    c.randomDirection();
-    cout << c << endl;
-    c.Move();
-    cout << c << endl;
-    c.randomDirection();
-    cout << c << endl;
-    c.Move();
-    cout << c << endl;
-    c.randomDirection();
-    cout << c << endl;
-    c.Move();
-    cout << c << endl;
 
     return 0;
 }
