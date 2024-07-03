46197fb66933036ead1eaab14fa8a04f600d5278
Sriram <sriramnvilvanathan@gmail.com>
2024-07-03 23:50:20 +0530

    updated the boilerplate/template for the rest of pgms

--- cppTemplate.cpp
+++ cppTemplate.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main()
 {
+    string str;
+
+    cout << "Enter a text to be check pallindrome: ";
+    getline(cin, str);
+
+    cout << "the string: '" << str;
     return 0;
 }
