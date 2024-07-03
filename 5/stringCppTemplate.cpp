8910e9a2024bc62cc26d52ffce3355323475b2f8
Sriram <sriramnvilvanathan@gmail.com>
2024-07-03 23:53:44 +0530

    minor update

---
+++ 5/stringCppTemplate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << "Enter a text to be check pallindrome: ";
+    getline(cin, str);
+
+    cout << "the string: '" << str;
+    return 0;
+}
---
+++ 5/string_largestWordInAString.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << "Enter a text to be check pallindrome: ";
+    getline(cin, str);
+
+    cout << "the string: '" << str;
+    return 0;
+}
