e06811521251a6548ecb47f39847a4ffef525ab8
Sriram <sriramnvilvanathan@gmail.com>
2024-07-04 00:19:05 +0530

    string program to find the biggest word in the sentance

--- 5/string_largestWordInAString.cpp
+++ 5/string_largestWordInAString.cpp
@@ -3,13 +3,43 @@
 
 using namespace std;
 
+string largest(string str)
+{
+    string large1, large2, temp;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        large2 += str[i];
+
+        if (isspace(str[i]) || i == str.length() - 1)
+        {
+            if (large1.length() < large2.length())
+            {
+                large1.clear();
+                large1 = large2;
+                large2.clear();
+            }
+            else if (large1.length() == large2.length())
+            {
+                temp = large1 + " " + large2;
+                large1.clear();
+                large1 = large2;
+                large2.clear();
+            }
+            else if (large1.length() > large2.length())
+                large2.clear();
+        }
+    }
+    return large1;
+}
+
 int main()
 {
     string str;
 
-    cout << "Enter a text to be check pallindrome: ";
+    cout << "Enter a line: ";
     getline(cin, str);
 
-    cout << "the string: '" << str;
+    cout << "the largest work in the string: '" << largest(str) << "'";
     return 0;
 }
